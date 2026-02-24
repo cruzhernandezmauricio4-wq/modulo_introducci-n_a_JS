3aec8108b35c62a64ce59b3779ed7acce1df69cb
Mauricio Cruz <cruzhernandezmauricio4@gmail.com>
2026-02-23 19:09:50 -0600

    primer commit

---
+++ tipos-de-datos.js
@@ -0,0 +1,9 @@
+console.log(typeof 42, typeof 'Veinticinco', typeof -666, typeof true, typeof 0, typeof "",typeof null, typeof undefined, typeof false);
+console.log(typeof"");
+/* estos son mis ejemplos*/
+/*aqui le agrega el 45 en la impresion, no hacer la operacion, se tiene que poner parentesis  */
+console.log(typeof 4+45);
+console.log(typeof(45+5));
+/* no deja poner caractweres guardados sin "" ejemplo si pones + sin eso es error  */
+console.log(typeof "%", typeof "°", typeof "<", typeof Gns);
+/*revisar si si guarda cambios*/ 
