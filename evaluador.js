c7dd7b41bb37f7ba628f76c7261a527380ed4fb5
Mauricio Cruz <cruzhernandezmauricio4@gmail.com>
2026-02-27 01:20:44 -0600

    agregar evaluador

---
+++ evaluador.js
@@ -0,0 +1,13 @@
+/*objetivo es evalaucar notas de un estudiantey decirle si paso o no*/
+let nota = 50;
+if (nota>= 90 && nota <=100){
+    console.log('Excelente');
+} else if ( nota >= 75 && nota <=89) {
+    console.log("Bien")
+} else if (nota >= 60 && nota <= 74){
+    console.log("Suficiente");
+} else if (nota >= 0 && nota < 60) {
+    console.log("No aprobdo");
+} else {
+    console.log("Valor no valido");
+}
